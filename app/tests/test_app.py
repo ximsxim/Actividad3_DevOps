86e337972414b9eb8ad9977441d22649491c2167
Fernanda Ximena Garcia Salazar <xcate682@gmail.com>
2026-04-17 11:46:58 -0600

    Ajusta import y comando de pruebas en GitHub Actions

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,13 +1,13 @@
-from app import app
+import app
 
 def test_home():
-    client = app.test_client()
+    client = app.app.test_client()
     response = client.get('/')
     assert response.status_code == 200
     assert response.json['message'] == "¡Hola desde Flask en Docker!"
 
 def test_health():
-    client = app.test_client()
+    client = app.app.test_client()
     response = client.get('/health')
     assert response.status_code == 200
     assert response.json['status'] == "ok"
